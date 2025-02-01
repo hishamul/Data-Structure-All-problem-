6d1170255a0991aac2f8500d0e0b2a913e499b82
hishamul <hishamulhaque4444@gmail.com>
2025-02-01 22:08:40 +0600

    i love

--- arrayInsertion.c
+++ arrayInsertion.c
@@ -8,9 +8,25 @@ int main(){
     scanf("%d",&pos);
     printf("Enter the Number");
     scanf("%d",&num);
-    if (size>arr[20])
+    printf("Enter the array element ");
+    for ( i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    if (pos>size)
     {
     printf("Overflow array");
+    }
+    for ( i = size-1; i >pos-1; i--)
+    {
+       arr[i+1]=arr[i];
+    }
+    arr[pos-1]=num;
+    size++;
+    for ( i = 0; i <size; i++)
+    {
+            printf("%d",arr[i]);
     }
     
     return 0;
