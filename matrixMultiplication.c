648da027f74e2608c7c6540bf8dcdb911117dc0c
hishamul <hishamulhaque4444@gmail.com>
2025-02-19 23:43:59 +0600

    2nd

--- matrixMultiplication.c
+++ matrixMultiplication.c
@@ -3,8 +3,10 @@
 int main(){
     int a[MAX][MAX];
     int arows,acolumns;
+    int brows,bcolumns;
     printf("Enter the rows and columns of the matrix a: ");
     scanf("%d%d", &arows,acolumns);
-    
+    printf("Enter the rows and columns of the matrix b: ");
+    scanf("%d%d", &brows,bcolumns);
     return 0;
 }
