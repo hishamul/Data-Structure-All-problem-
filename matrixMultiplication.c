fd5653be9ecb6fa49656bc88c0f1d0b279bb08bb
hishamul <hishamulhaque4444@gmail.com>
2025-02-20 00:14:00 +0600

    mm

--- matrixMultiplication.c
+++ matrixMultiplication.c
@@ -3,37 +3,38 @@
 int main(){
     int a[MAX][MAX];
     int b[MAX][MAX];
+    int product[MAX][MAX];
+int sum=0;
     int arows,acolumns;
     int brows,bcolumns;
     printf("Enter the rows and columns of the matrix a: ");
-    scanf("%d%d", &arows,acolumns);
-    
+    scanf("%d%d", &arows,&acolumns);
+
 printf("Enter the elements of matrix a:");
 for (int  i = 0; i <arows; i++)
 {
-    for (int j = 0; j <acolumns; i++)
+    for (int j = 0; j <acolumns; j++)
     {
         scanf("%d",&a[i][j]);
     }
-    
+
 }
 printf("Enter the rows and columns of the matrix b: ");
-    scanf("%d%d", &brows,bcolumns);
+    scanf("%d%d", &brows,&bcolumns);
 
 printf("Enter the elements of matrix b:");
 for (int  i = 0; i <arows; i++)
 {
-    for (int j = 0; j <acolumns; i++)
+    for (int j = 0; j <acolumns; j++)
     {
         scanf("%d",&a[i][j]);
     }
-    
+
 }
-int product[MAX][MAX];
-int sum=0;
+
 for (int i = 0; i <arows; i++)
 {
-    for (int j = 0; j < bcolumns; i++)
+    for (int j = 0; j < bcolumns; j++)
     {
         for (int  k = 0; i < brows; k++)
         {
@@ -42,7 +43,16 @@ for (int i = 0; i <arows; i++)
         product[i][j]=sum;
         sum=0;
     }
-    
+
+}
+printf("resultant matrix\n");
+for (int  i = 0; i <arows; i++)
+{
+    for (int j = 0; j < bcolumns; j++)
+    {
+        printf("%d",product[i][j]);
+    }
+  printf("\n");
 }
 
     return 0;
